6e5f65c894ce8a842b38664bcdc2e093aa008b2e
coolerAaron <aaron.andrews1996@gmail.com>
2022-04-22 00:33:59 -0500

    reorginazation~

--- src/ResponsiveLayout/ResponsiveLayout.tsx
+++ src/ResponsiveLayout/ResponsiveLayout.tsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { ConditionalWraper } from "./ConditionalWrapper";
 
+//Hi, how ya doin, gonna walk through this code a bit
+
+//Just setting up some prop interfaces for type safety stuff and all that
 interface ValueBoxProps {
   value: string | number;
   color: string;
@@ -14,6 +17,8 @@ interface FlexBoxProps {
   height?: string;
 }
 
+//Using styled components here, my preferred css-in-js option
+//It handles breakpoint stuff real smoothly as well
 const FlexBox = styled.div<FlexBoxProps>`
   gap: ${(props) => props.gap || "1.5rem"};
   margin: ${(props) => props.margin};
@@ -23,13 +28,58 @@ const FlexBox = styled.div<FlexBoxProps>`
   justify-content: space-evenly;
 `;
 
+//this seems a lil redundant, but it helps me recognize if a flex box is horizontal or vertical as I'm setting up the layout
 const HBox = FlexBox;
 const VBox = styled(FlexBox)`
   flex-direction: column;
 `;
 
+//setting breakpoint here --> mobile < 600px < desktop
+const breakPoint = "(max-width: 600px)";
+
+const ColorBox = styled.div<{ flexGrow: number }>`
+  display: flex;
+  background: ${(props) => props.color};
+  flex-grow: ${(props) => props.flexGrow || 1};
+  @media ${breakPoint} {
+    flex-grow: 1;
+  }
+`;
+
+const RatioBox = styled(FlexBox)`
+  @media ${breakPoint} {
+    flex-grow: 1.5;
+  }
+`;
+
+const RatioVBox = styled(RatioBox)`
+  flex-direction: column;
+`;
+
+const TopPane = styled(FlexBox)`
+  margin: 0.5rem;
+  flex-grow: 1.5;
+  @media ${breakPoint} {
+    flex-direction: column;
+    flex-grow: 1;
+    ${RatioVBox}:nth-child(2) {
+      flex-direction: column-reverse;
+    }
+  }
+`;
+
+const BottomPane = styled(FlexBox)`
+  margin: 0.5rem;
+  @media ${breakPoint} {
+    flex-direction: column;
+  }
+`;
+
 export const ResponsiveLayout = () => {
-  const breakPoint = "(max-width: 600px)";
+
+  //I wanted to mostly rely on media queries within a styled component
+  //, but there was some conditional logic i wanted to pull off based on the breakpoint
+  // so I setup a hook to also respond to media queries
   const mediaMatch = window.matchMedia(breakPoint);
   const [matches, setMatches] = useState(mediaMatch.matches);
 
@@ -39,7 +89,7 @@ export const ResponsiveLayout = () => {
     return () => mediaMatch.removeEventListener("change", handler);
   });
 
-  //enter color map here
+  //this is just an array of colors that I'll shuffle on click of a box
   const [colorPool, setColorPool] = useState([
     "#0289DF",
     "#E53458",
@@ -57,50 +107,13 @@ export const ResponsiveLayout = () => {
     setColorPool([...colorPool].sort(() => Math.random() - 0.5));
   };
 
-  const RatioBox = styled(FlexBox)`
-    @media ${breakPoint} {
-      flex-grow: 1.5;
-    }
-  `;
-
-  const RatioVBox = styled(RatioBox)`
-    flex-direction: column;
-  `;
-
-  const TopPane = styled(FlexBox)`
-    margin: 0.5rem;
-    flex-grow: 1.5;
-    @media ${breakPoint} {
-      flex-direction: column;
-      flex-grow: 1;
-      ${RatioVBox}:nth-child(2) {
-        flex-direction: column-reverse;
-      }
-    }
-  `;
-
-  const BottomPane = styled(FlexBox)`
-    margin: 0.5rem;
-    @media ${breakPoint} {
-      flex-direction: column;
-    }
-  `;
-
   const ValueBox = ({
     value,
     color,
     flexGrow,
   }: ValueBoxProps & FlexBoxProps) => {
-    const ColorBox = styled.div`
-      display: flex;
-      background: ${color};
-      flex-grow: ${flexGrow || 1};
-      @media ${breakPoint} {
-        flex-grow: 1;
-      }
-    `;
     return (
-      <ColorBox onClick={shuffleColorPool}>
+      <ColorBox flexGrow={flexGrow} color={color} onClick={shuffleColorPool}>
         <p
           style={{
             margin: "auto",
@@ -129,6 +142,14 @@ export const ResponsiveLayout = () => {
         </RatioVBox>
       </TopPane>
       <BottomPane>
+        {/* Yeah this looks weird I know, I'm conditionally rendering the "7" box
+            on top of the rest of bottom half of the layout when in the mobile layout.
+            I also have this conditional wrapper class that adds a horizontal flex box around
+            everything else to put the "5/6" and "7/8" examples together like in the example.
+
+            Most hacky looking thing here, and I think using something like css grid might've been
+            better suited here.
+        */}
         {matches ? <ValueBox value={7} color={colorPool[6]} /> : undefined}
         <ConditionalWraper
           condition={matches}
